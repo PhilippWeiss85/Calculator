470b5cfc4af473aff87fe05c2231db79c9d682c3
Philipp <p.weiss85gmx.de>
2023-02-09 10:03:03 +0100

    update styling

--- src/App.js
+++ src/App.js
@@ -2,31 +2,27 @@ import "./App.css";
 
 function App() {
   return (
-    <section>
-      <article>
-        <h1>Count</h1>
-      </article>
-      <article>
-        <button>AC</button>
-        <button>+/_</button>
-        <button>%</button>
-        <button>...</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>x</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>0</button>
-        <button>,</button>
-        <button>=</button>
-      </article>
+    <section className="grid">
+      <article className="grid__output">output</article>
+      <button>AC</button>
+      <button>+/_</button>
+      <button>%</button>
+      <button>...</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button className="grid__span-two grid__bottom-border-left">0</button>
+      <button>,</button>
+      <button className="grid__bottom-border-right">=</button>
     </section>
   );
 }
