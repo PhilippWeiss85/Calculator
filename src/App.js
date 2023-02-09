dbd9c548509680ee45b3fa4134d2f0c307397180
Philipp <p.weiss85gmx.de>
2023-02-09 11:06:42 +0100

    add useReducer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,50 @@ import "./App.css";
 import "./components/Button.css";
 import { useReducer } from "react";
 
+const OPERANDS = {
+  ADD: "add",
+  SUBSTRACT: "substract",
+  DIVIDE: "divide",
+  MULTIPLY: "multiply",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case OPERANDS.ADD:
+      return { count: state.count + 1 };
+    case OPERANDS.SUBSTRACT:
+      return state.count - 1;
+    case OPERANDS.DIVIDE:
+      return state + 13;
+    case OPERANDS.MULTIPLY:
+      return state - 13;
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 1 });
+
+  function add() {
+    dispatch({ type: OPERANDS.ADD });
+  }
+  function substract() {
+    dispatch({ type: OPERANDS.SUBSTRACT });
+  }
+  function divide() {
+    dispatch({ type: OPERANDS.DIVIDE });
+  }
+  function multiply() {
+    dispatch({ type: OPERANDS.MULTIPLY });
+  }
+  function reset() {
+    dispatch();
+  }
+
   return (
     <section className="grid">
-      <article className="grid__output">12321451</article>
+      <article className="grid__output">{state.count}</article>
       <button className="grid__operands-darkgrey">AC</button>
       <button className="grid__operands-darkgrey">+/_</button>
       <button className="grid__operands-darkgrey">%</button>
@@ -21,7 +61,9 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button className="grid__operands-orange">+</button>
+      <button onClick={add} className="grid__operands-orange">
+        +
+      </button>
       <button className="grid__span-two grid__bottom-border-left">0</button>
       <button>,</button>
       <button className="grid__bottom-border-right grid__operands-orange">
