5f25c405903653e99dab55241a62f68d867c46bf
Philipp <p.weiss85gmx.de>
2023-02-09 10:12:07 +0100

    update styling

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 function App() {
   return (
     <section className="grid">
-      <article className="grid__output">output</article>
-      <button>AC</button>
-      <button>+/_</button>
-      <button>%</button>
-      <button>...</button>
+      <article className="grid__output">12321451</article>
+      <button className="grid__operands-darkgrey">AC</button>
+      <button className="grid__operands-darkgrey">+/_</button>
+      <button className="grid__operands-darkgrey">%</button>
+      <button className="grid__operands-orange">...</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button>x</button>
+      <button className="grid__operands-orange">x</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>-</button>
+      <button className="grid__operands-orange">-</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>+</button>
+      <button className="grid__operands-orange">+</button>
       <button className="grid__span-two grid__bottom-border-left">0</button>
       <button>,</button>
-      <button className="grid__bottom-border-right">=</button>
+      <button className="grid__bottom-border-right grid__operands-orange">
+        =
+      </button>
     </section>
   );
 }
